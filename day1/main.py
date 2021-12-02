230155d0f5eed7c2550fe219e327c14532131d35
Spencer Yeager <yeager.spencer@gmail.com>
2021-12-02 00:38:50 -0700

    day1 actually complete

--- day1/main.py
+++ day1/main.py
@@ -3,19 +3,36 @@ import numpy as np
 
 def main():
     # input file
-    vals = np.genfromtxt("day1/input.csv", delimiter=",")
-    
-    true_count = 0
-    comp_val = vals[0]
-    
-    index = 1
-    while index < len(vals):
-        if (vals[index] - comp_val) > 0:
-            true_count += 1
-        comp_val = vals[index]
-        index += 1
+    file = open('day1/input.csv', 'r')
+    val_list = []
+    for i in file:
+        val_list.append(int(i))
 
+    # part 1
+    true_count = 0
+    comp_val = val_list[0]
+    index = 1
+    while index < len(val_list):
+        if (val_list[index] - comp_val) > 0:
+            true_count += 1
+        comp_val = val_list[index]
+        index += 1
     print(true_count)
+
+    # part 2
+    true_count = 0
+    # only really need to compare the lower edge of window 1
+    # with upper edge of window 2. overlapping will always
+    # be the same, so sum is dependent only on those edges
+    lower_edge = 0
+    upper_edge = 3
+    while upper_edge < len(val_list):
+        if val_list[lower_edge] < val_list[upper_edge]:
+            true_count += 1
+        lower_edge += 1
+        upper_edge += 1
+    print(true_count)
+    
 
 if __name__ == '__main__':
     main()
