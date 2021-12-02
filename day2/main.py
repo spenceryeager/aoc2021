f324934d3626a5092625113ce27b45fa0a90171d
Spencer Yeager <yeager.spencer@gmail.com>
2021-12-02 01:22:01 -0700

    day2 part 1 done

--- day2/main.py
+++ day2/main.py
@@ -1,7 +1,30 @@
-def main():
-    print('hello!')
 
+def main():
+    # file opening
+    file = open("day2/input", "r")
+    vals = []
+    for value in file:
+        vals.append(value.strip("\n"))
+    file.close()
+
+    # part 1
+    x_pos = 0
+    z_up = 0
+    z_down = 0
+
+    for i in vals:
+        if i[0] == 'f':
+            x_pos += int(i[-1])
+
+        elif i[0] == 'u':
+            z_up += int(i[-1])
+
+        elif i[0] == 'd':
+            z_down += int(i[-1])
+    
+    print(x_pos * (z_down - z_up))
+
+    # part 2
 
 if __name__ == "__main__":
     main()
-    
