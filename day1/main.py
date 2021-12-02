dc887b37af31f25fb30ab0d92733cb866ca8f5c7
Spencer Yeager <yeager.spencer@gmail.com>
2021-12-01 23:45:47 -0700

    added reqs, day 1 complete

--- day1/main.py
+++ day1/main.py
@@ -1,1 +1,21 @@
-print('test')
+import numpy as np
+
+
+def main():
+    # input file
+    vals = np.genfromtxt("day1/input.csv", delimiter=",")
+    
+    true_count = 0
+    comp_val = vals[0]
+    
+    index = 1
+    while index < len(vals):
+        if (vals[index] - comp_val) > 0:
+            true_count += 1
+        comp_val = vals[index]
+        index += 1
+
+    print(true_count)
+
+if __name__ == '__main__':
+    main()
